6d066a33661d4cd949e14cfac21f6b340c06376a
Mateusz <75246331+Zikuz@users.noreply.github.com>
2021-05-23 18:21:05 +0200

    media

--- scripts/main.js
+++ scripts/main.js
@@ -3,17 +3,69 @@ const mapExpansion = [...document.querySelectorAll('.img-expansion')]
 
 const btnNormal = document.querySelector('.btn-normal')
 const btnExpansion = document.querySelector('.btn-expansion')
+
 const desertNormal = document.querySelector('.desert-normal')
-const desertExpansion = document.querySelector('.desert-expansion')
-const desertExpansion2 = document.querySelector('.desert-expansion2')
+const desertExpansion = [...document.querySelectorAll('.desert-expansion')]
 
-btnExpansion.addEventListener('click', () => {
+const randomMap = (images) => {
+    for (let i = images.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [images[i], images[j]] = [images[j], images[i]];
+    }
+}
 
+const clearMapNormal = () => {
     mapNormal.forEach(image => {
         image.src = "";      
     });
 
     desertNormal.src = "";
+}
+
+const clearMapExpansion = () => {
+    mapExpansion.forEach(image => {
+        image.src = "";      
+    });
+
+    desertExpansion.forEach(image => {
+        image.src = "";      
+    });
+}
+
+const generateMapNormal = () => {
+    let images = [
+        'images/clay.png', 
+        'images/clay.png', 
+        'images/clay.png', 
+        'images/wool.png',
+        'images/wool.png',
+        'images/wool.png',
+        'images/wool.png',
+        'images/wood.png',
+        'images/wood.png',
+        'images/wood.png',
+        'images/wood.png',
+        'images/stone.png',
+        'images/stone.png',
+        'images/stone.png',
+        'images/grain.png',
+        'images/grain.png',
+        'images/grain.png',
+        'images/grain.png'
+    ] 
+    
+    randomMap(images);
+    let index = 0;
+    
+    mapNormal.forEach(image => {
+        image.src = images[index];   
+        index += 1;
+    });
+
+    desertNormal.src = 'images/desert.png';
+}
+
+const generateMapExpansion = () => {
 
     let images = [
         'images/clay.png', 
@@ -46,36 +98,32 @@ btnExpansion.addEventListener('click', () => {
         'images/grain.png'
     ] 
 
-    function randomMap(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-    }
-
     randomMap(images);
+
     let index = 0;
 
     mapExpansion.forEach(image => {
         image.src = images[index];      
-        index+=1;
+        index += 1;
     });
-    
-    desertExpansion.src = 'images/desert.png';
-    desertExpansion2.src = 'images/desert.png';
 
+    desertExpansion.forEach(image => {
+        image.src = 'images/desert.png';      
+    });
+}
+
+btnExpansion.addEventListener('mousemove', () => {
+    clearMapNormal()
+    generateMapExpansion();
+})
+
+btnNormal.addEventListener('mousemove', () => {
+    clearMapExpansion()
+    generateMapNormal();
 })
 
 
-btnNormal.addEventListener('click', () => {
-
-    mapExpansion.forEach(image => {
-        image.src = "";      
-    });
-    
-    desertExpansion.src = "";
-    desertExpansion2.src = "";
-
+const generateStartMap = () => {
     let images = [
         'images/clay.png', 
         'images/clay.png', 
@@ -96,21 +144,21 @@ btnNormal.addEventListener('click', () => {
         'images/grain.png',
         'images/grain.png'
     ] 
-
-    function randomMap(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-    }
-
+    
     randomMap(images);
     let index = 0;
 
+    
     mapNormal.forEach(image => {
         image.src = images[index];   
-        index+=1;
+        index += 1;
     });
 
     desertNormal.src = 'images/desert.png';
-})
+}
+
+window.addEventListener('load', () => {
+    generateStartMap();
+});
+
+
